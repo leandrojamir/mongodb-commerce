8b44afd71f591c7a3ea9c91f78dba8a50ff7e444
leandrojamir <leandrojamir@hotmail.com>
2023-02-18 15:50:52 -0300

    feat: desafio26

---
+++ challenges/desafio26.js
@@ -0,0 +1,17 @@
+// 26 - Adicione o valor contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40
+// Para isso, escreva no arquivo desafio26.js duas queries, nesta ordem:
+// Crie uma query que faça a adição do valor contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
+
+db.produtos
+  .updateMany(
+    { valoresNutricionais: { $elemMatch: {
+      tipo: "sódio",
+      percentual: { $gt: 20, $lt: 40 },
+    } } },
+    { $push: { tags: "contém sódio" } },
+  );
+  
+// Crie uma query que retorne o nome e tags de todos os documentos.
+
+  db.produtos
+  .find({ }, { _id: 0, nome: true, tags: true });
